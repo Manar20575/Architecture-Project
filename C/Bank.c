1fe3490746a9ceaf4fa4491de261a5ae2fa10f53
menna5 <76221962+menna5@users.noreply.github.com>
2022-05-14 19:46:33 +0200

    Update Bank.c

--- C/Bank.c
+++ C/Bank.c
@@ -4,14 +4,15 @@ int pcounter (bool updown, int pcount);
 int tcounter (bool updown, int tcount);
 int emptyflag (int pcount);
 int fullflag (int pcount);
-void p_alarmflag (int pcount);
-void t_alarmflag (int tcount);
+bool p_alarmflag (int pcount);
+bool t_alarmflag (int tcount);
 int waiting_time (int pcount, int tcount);
 int main ()
 {
     bool reset;
     bool full, empty, alarm;
     int pcount = 0, tcount = 0, wtime = 0, exit, check;
+    int tcount_tst, pcount_tst;
     printf("Welcome to our bank system\n");
     while (true)
     {
@@ -25,23 +26,28 @@ int main ()
         switch (check)
         {
             case 3:
-                tcount = tcounter(1, tcount);
+                tcount_tst = tcounter(1, tcount);
+                tcount = tcount_tst != 4 ? tcount_tst : tcount;
                 wtime = waiting_time (pcount, tcount);
                 break;
             case -3:
-                tcount = tcounter(0, tcount);
+                tcount_tst = tcounter(0, tcount);
+                tcount = tcount_tst != -1 ? tcount_tst : tcount;
                 wtime = waiting_time (pcount, tcount);
                 break;
             case 7:
-                pcount = pcounter(1, pcount);
+                pcount_tst = pcounter(1, pcount);
+                pcount = pcount_tst != 8 ? pcount_tst : pcount;
                 wtime = waiting_time (pcount, tcount);
                 break;
             case -7:
-                pcount = pcounter(0, pcount);
+                pcount_tst = pcounter(0, pcount);
+                pcount = pcount_tst != -1 ? pcount_tst : pcount;
                 wtime = waiting_time (pcount, tcount);
                 break;
             case 0:
                 pcount = 0;
+                tcount = 0;
                 wtime = waiting_time (pcount, tcount);
                 break;
             case -1:
@@ -54,8 +60,14 @@ int main ()
         {
             break;
         }
-        p_alarmflag (pcount);
-        t_alarmflag (tcount);
+        if (p_alarmflag (pcount_tst))
+        {
+            pcount_tst++;
+        }
+        if (t_alarmflag (tcount_tst))
+        {
+            tcount_tst++;
+        }
         if (emptyflag (pcount))
         {
             printf("The queue is empty.\n");
@@ -118,27 +130,33 @@ int fullflag (int pcount)
 {
     return pcount == 7;
 }
-void p_alarmflag (int pcount)
+bool p_alarmflag (int pcount)
 {
     if (pcount == 8)
     {
         printf("The maximum number of people we can add is 7, Now you only can remove.\n");
+        return 1;
     }
     else if (pcount == -1)
     {
         printf("There is no one in the queue to leave.\n");
+        return 1;
     }
+    return 0;
 }
-void t_alarmflag (int tcount)
+bool t_alarmflag (int tcount)
 {
     if (tcount == 4)
     {
         printf("The maximun number of tellers is 3, Now you only can remove.\n");
+        return 1;
     }
     else if (tcount == -1)
     {
         printf("There is no tellers to remove.\n");
+        return 1;
     }
+    return 0;
 }
 int waiting_time (int pcount, int tcount)
 {
